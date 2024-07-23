2e4125b0191d4b5415c428397625383728bca520
Jose <dstryr77@gmail.com>
2024-07-23 08:28:26 -0500

    accent color in checkbox

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function Login() {
     return(
         <main className="max-w-5xl mx-auto h-screen flex items-center">
-            <section className="h-[60%] grid grid-cols-2 items-center my-auto gap-7 px-10 py-5 bg-white">
+            <section className="h-[60%] grid grid-cols-2 items-center my-auto gap-7 pl-10 pr-5 py-5 bg-white shadow-xl">
                 <section className="flex flex-col h-full pl-5 gap-6">
                     <h1 className="text-primary text-2xl font-semibold">Wellness</h1>
                     <h2><strong className="text-xl">Weolcome Back</strong></h2>
@@ -19,8 +19,8 @@ export function Login() {
                         </div>
                     </form>
                     <div className="flex justify-between">
-                        <div className="flex gap-2">
-                            <input type="checkbox" name="" id="" className="checked:bg-primary" /><label htmlFor="">Remenber me</label>
+                        <div className="flex gap-2 ">
+                            <input type="checkbox" name="" id="" className="accent-current text-primary" /><label htmlFor="">Remenber me</label>
                         </div>
                         <Link to="/" className="text-primary font-semibold">Forgot password?</Link>
                     </div>
@@ -29,7 +29,7 @@ export function Login() {
                         <p>No registered yet? <Link to={"/register"} className="text-primary font-semibold">Create account</Link> </p>
                     </div>
                 </section>
-                <img src="https://img.freepik.com/fotos-premium/imagen-llamativa-mujer-joven-plantas-que-dan-toque-naturaleza-su-rostro-aigenerated_796128-2753.jpg" alt="imagen login" className="size-full object-cover" />
+                <img src="https://img.freepik.com/fotos-premium/imagen-llamativa-mujer-joven-plantas-que-dan-toque-naturaleza-su-rostro-aigenerated_796128-2753.jpg" alt="imagen login" className="h-full object-cover" />
             </section>
         </main>
     )
